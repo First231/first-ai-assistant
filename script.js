6a811b026c5b2555192ea30a8714df34383f3172
First231 <116333488+First231@users.noreply.github.com>
2026-05-11 18:08:10 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+async function send(){
+
+let message = document.getElementById("message").value
+
+let res = await fetch("https://first-ai-assistant-ai-chatbot.onrender.com/chat",{
+method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
+body: JSON.stringify({
+message: message
+})
+})
+
+let data = await res.json()
+
+let chat = document.getElementById("chat")
+
+chat.innerHTML += "<p><b>You:</b> "+message+"</p>"
+chat.innerHTML += "<p><b>AI:</b> "+data.response+"</p>"
+
+document.getElementById("message").value = ""
+
+}
