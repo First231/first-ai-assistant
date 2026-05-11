fd76142503a28f8569f469d4deaa98747d12e61e
First231 <116333488+First231@users.noreply.github.com>
2026-05-11 18:27:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ async function send(){
 
 let message = document.getElementById("message").value
 
-let res = await fetch("https://first-ai-assistant-ai-chatbot.onrender.com",{
+let res = await fetch("https://first-ai-assistant-ai-chatbot.onrender.com/chat", {
 method:"POST",
 headers:{
 "Content-Type":"application/json"
